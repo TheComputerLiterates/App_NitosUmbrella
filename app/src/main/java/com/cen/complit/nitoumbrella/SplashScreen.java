460cb28e829a2d70a321cc38f0037d42a5710fb6
Aaron <aak12b@my.fsu.edu>
2015-01-15 16:01:01 -0500

    Initial commit

---
+++ app/src/main/java/com/cen/complit/nitoumbrella/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.cen.complit.nitoumbrella;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends Activity {
+
+    private static int TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            //show splash screen for 3 seconds
+            //then launch main
+
+            @Override
+            public void run() {
+                //this launches main when timer runs out
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, TIME_OUT);
+    }
+//screwthis
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
