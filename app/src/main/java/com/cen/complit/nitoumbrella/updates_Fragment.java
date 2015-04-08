849e28277b1c91559ccb0b767ee497a202136bc7
atm11c <atm11c@my.fsu.edu>
2015-04-08 12:25:15 -0400

    View Profile changes

--- app/src/main/java/com/cen/complit/nitoumbrella/updates_Fragment.java
+++ app/src/main/java/com/cen/complit/nitoumbrella/updates_Fragment.java
@@ -1,16 +1,21 @@
 package com.cen.complit.nitoumbrella;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Andrew on 1/28/2015.
  */
 public class updates_Fragment extends Fragment{
+
+    String[] dummyData = new String[] {"DumDum", "HerpDerp", "Dingus"};
     View rootview;
     @Nullable
     @Override
@@ -18,4 +23,5 @@ public class updates_Fragment extends Fragment{
         rootview = inflater.inflate(R.layout.updates_layout, container, false);
         return rootview;
     }
+
 }
