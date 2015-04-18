6a86f9300257f63df5302e8d073cdbd6aa6b8c17
atm11c <atm11c@my.fsu.edu>
2015-04-18 12:40:19 -0400

    View Profile Completed

--- app/src/main/java/com/cen/complit/nitoumbrella/viewProfile_Fragment.java
+++ app/src/main/java/com/cen/complit/nitoumbrella/viewProfile_Fragment.java
@@ -10,9 +10,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Andrew on 1/28/2015.
@@ -21,17 +30,23 @@ import org.json.JSONObject;
 
 public class viewProfile_Fragment extends Fragment{
 
-    private static String url = "http://fsuhvz.com/api/abc13ef/viewprofile/";
-    private static String TAG_PROFILE = "profile";
-    private static String TAG_FIRST_NAME = "first";
-    private static String TAG_LAST_NAME = "second";
+    public String url = "http://hvz.sabaduy.com/api/" + ServiceHandler.APIKEY + "/user/profile";
+    private static String TAG_SUCCESS = "success";
+    private static String TAG_BODY = "body";
+    private static String TAG_FIRST = "firstName";
+    private static String TAG_LAST = "lastName";
     private static String TAG_EMAIL = "email";
-    private static String TAG_HVZID = "hvzid";
-    private static String TAG_ROLE = "role";
-    private static String name = "";
-    private static String id = "";
-    private static String role = "";
-    private static String email = "";
+    private static String TAG_HVZID = "HVZID";
+    private static String TAG_ROLE = "roleName";
+    private boolean status = false;
+    private String name,
+    id,
+    hvzid,
+    role,
+    email,
+    first,
+    last,
+    logindata;
 
     JSONArray profileInfo = null;
     View rootview;
@@ -40,7 +55,27 @@ public class viewProfile_Fragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         rootview = inflater.inflate(R.layout.viewprofile_layout, container, false);
 
-        //new GetProfileInfo().execute();
+        String filename = "session";
+        File myFile = new File(getActivity().getFilesDir(), filename);
+        if (myFile.exists()) {
+            FileInputStream inputStream;
+            try {
+                inputStream = getActivity().openFileInput(filename);
+                byte[] login_test = new byte[inputStream.available()];
+                while (inputStream.read(login_test) != -1) {
+                }
+                logindata = new String(login_test);
+                String[] separated = logindata.split(";");
+                id = separated[2];
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        new GetProfileInfo().execute();
 
         return rootview;
     }
@@ -48,25 +83,35 @@ public class viewProfile_Fragment extends Fragment{
     private class GetProfileInfo extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... arg0) {
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
             //create service handler
+
+            params.add(new BasicNameValuePair("id", id));
+
             ServiceHandler sh = new ServiceHandler();
 
             //Make request to url
-            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.POST, params);
+            Log.d("PROFILE", jsonStr);
             if (jsonStr != null) {
                 try {
                     JSONObject jsonObject = new JSONObject(jsonStr);
-
-                    profileInfo = jsonObject.getJSONArray(TAG_PROFILE);
-
-                    //Log.d("Derp", "made it here");
-
-                    JSONObject c = profileInfo.getJSONObject(0);
-                    name = c.getString(TAG_FIRST_NAME + "" + TAG_LAST_NAME);
-                    id = c.getString(TAG_HVZID);
-                    email = c.getString(TAG_EMAIL);
-                    role = c.getString(TAG_ROLE);
+                    status = jsonObject.getBoolean(TAG_SUCCESS);
+                    if(status){
+                        JSONObject body = jsonObject.getJSONObject(TAG_BODY);
+                        first = body.getString(TAG_FIRST);
+                        last = body.getString(TAG_LAST);
+                        name = first + " " + last;
+                        email = body.getString(TAG_EMAIL);
+                        hvzid = body.getString(TAG_HVZID);
+                        role = body.getString(TAG_ROLE);
+                    }
+                    else{
+                        name = "error";
+                        email = "connecting";
+                        hvzid = "to";
+                        role = "server";
+                    }
 
 
                 } catch (JSONException e) {
@@ -89,7 +134,7 @@ public class viewProfile_Fragment extends Fragment{
 
             //Update stuff
             pn.setText(name);
-            hid.setText(id);
+            hid.setText(hvzid);
             em.setText(email);
             ro.setText(role);
         }
